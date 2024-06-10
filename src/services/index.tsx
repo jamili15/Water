8ea8e6ed9068843ee1020c039d1ca7aaf452bda4
worgflores <worgflores@gmail.com>
2024-06-10 11:36:30 +0800

    initial commit

---
+++ src/services/index.tsx
@@ -0,0 +1,7 @@
+import * as CloudPartnerService from "../common/services/CloudPartnerService";
+import * as OTPService from "../common/services/OTPService";
+
+export const services = {
+  OTPService,
+  CloudPartnerService,
+};
