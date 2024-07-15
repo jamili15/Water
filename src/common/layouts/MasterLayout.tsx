a24b97ce3689b11b25bb5143fd7d4a1b3b28a22c
James Kevin Jamili <jameskevin.ramesessystems@gmail.com>
2024-07-15 14:30:07 +0800

    updated footer and masterlayout

--- src/common/layouts/MasterLayout.tsx
+++ src/common/layouts/MasterLayout.tsx
@@ -2,29 +2,38 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import { Url } from "next/dist/shared/lib/router/router";
+import { usePartnerContext } from "../components/Email/PartnerModel";
 
 interface HomeProps {
   children: React.ReactNode;
   lgucaption?: string | undefined;
   lguLogo: string;
-  href?: Url;
 }
 
 const MasterLayout: React.FC<HomeProps> = ({
   children,
   lgucaption,
   lguLogo,
-  href,
 }) => {
+  const { id } = usePartnerContext();
+
   return (
     <div className="flex flex-col min-h-screen bg-[#F5F5F5]">
-      <Header lguLogo={lguLogo} lgucaption={lgucaption} href={href} />
+      <Header
+        lguLogo={lguLogo}
+        lgucaption={lgucaption}
+        href={`http://192.168.2.9:9090/partners/${id}`}
+      />
       <div className="flex flex-col flex-1 pt-20">
         <main className="w-full flex justify-center items-start py-5 flex-1">
           {children}
         </main>
       </div>
-      <Footer copyright={"@Copyright 2024 Filipizen"} />
+      <Footer
+        copyright={"@Copyright 2024 "}
+        filipizen="Filipizen"
+        href={`http://192.168.2.9:9090/partners`}
+      />
     </div>
   );
 };
